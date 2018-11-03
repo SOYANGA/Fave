63a7aabc8acdfa25e2124e14fddb2fe0a15fb1cd
SOYANGA <321830735@qq.com>
2018-11-03 23:43:04 +0800

    猴子吃桃子游戏

---
+++ 猴子吃桃子游戏/Work5_6.java
@@ -0,0 +1,11 @@
+public class Work5_6 {
+    public static void main(String[] args) {
+        int count = 1;
+        int days = 9;
+        for (days = 9; days > 0; days--) {
+            count = (count + 1) * 2;
+        }
+        System.out.println(count);
+    }
+
+}
