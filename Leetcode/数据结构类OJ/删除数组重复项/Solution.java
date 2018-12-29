2f94db05f99ce7611eea5b1c36afe7d45087f85d
SOYANGA <321830735@qq.com>
2018-12-30 00:38:08 +0800

    Leetcode/数据结构类OJ/删除数组重复项

---
+++ Leetcode/数据结构类OJ/删除数组重复项/Solution.java
@@ -0,0 +1,16 @@
+package removeDuplicates;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        if(nums.length==0){
+            return 0;
+        }
+        for(int i = 1;i<nums.length;i++){
+            if(nums[j]!=nums[i]){
+                nums[++j]=nums[i];
+            }
+        }
+        return j+1;
+    }
+}
