81eea30f2c255f4483fa110466b45e15df93ca81
SOYANGA <321830735@qq.com>
2019-07-13 00:11:09 +0800

    剑指Offer-15二进制中 1 的个数-Integer().bitCount(1)直接计算

---
+++ 剑指Offer/15二进制中 1 的个数/Solution2.java
@@ -0,0 +1,15 @@
+package numberOf1;
+
+/**
+ * @program: TSRTOffer
+ * @Description:
+ * @Author: SOYANGA
+ * @Create: 2019-07-12 17:09
+ * @Version 1.0
+ */
+public class Solution2 {
+
+    public int NumberOf1(int n) {
+        return Integer.bitCount(n);
+    }
+}
