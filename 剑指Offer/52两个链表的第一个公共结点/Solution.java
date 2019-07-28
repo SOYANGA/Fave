a13dbe8589aea0f347035cdeee533d21b5dadf91
SOYANGA <321830735@qq.com>
2019-07-29 00:13:52 +0800

    剑指Offer-52两个链表的第一个公共结点-数学求路径长度

---
+++ 剑指Offer/52两个链表的第一个公共结点/Solution.java
@@ -0,0 +1,44 @@
+package findFirstCommonNode;
+
+/**
+ * @program: TSRTOffer
+ * @Description: 52两个链表的第一个公共结点
+ * @Author: SOYANGA
+ * @Create: 2019-08-02 00:30
+ * @Version 1.0
+ */
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode p1 = pHead1;
+        ListNode p2 = pHead2;
+        while (p1 != p2) {
+            p1 = (p1 == null) ? pHead2 : p1.next;
+            p2 = (p2 == null) ? pHead1 : p2.next;
+        }
+        return p1;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
+/*
+设 A 的长度为 a + c，B 的长度为 b + c，其中 c 为尾部公共部分长度，可知 a + c + b = b + c + a。
+
+当访问链表 A 的指针访问到链表尾部时，令它从链表 B 的头部重新开始访问链表 B；同样地，当访问链表 B 的指针访问到链表尾部时，
+令它从链表 A 的头部重新开始访问链表 A。这样就能控制访问 A 和 B 两个链表的指针能同时访问到交点。
+ */
