678f67fad7430a949b6365a83cc427527bce3951
SOYANGA <321830735@qq.com>
2019-08-04 16:24:27 +0800

    剑指Offer-60n 个骰子的点数-dp利用 dp[i][j] = dp[i - 1][j - (1 ~ min(j - 1,face) )]

---
+++ 剑指Offer/60n 个骰子的点数/Solution.java
@@ -0,0 +1,48 @@
+package dicesSum;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: TSRTOffer
+ * @Description: 60n个骰子的点数 动态规划
+ * @Author: SOYANGA
+ * @Create: 2019-08-02 01:43
+ * @Version 1.0
+ */
+public class Solution {
+    /**
+     * @param n an integer
+     * @return a list of Map.Entry<sum, probability>
+     */
+    public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+        final int face = 6;
+        final int pointNum = face * n;
+        long[][] dp = new long[n + 1][pointNum + 1];
+
+        for (int i = 1; i <= face; i++)
+            dp[1][i] = 1;
+
+        for (int i = 2; i <= n; i++)
+            for (int j = i; j <= pointNum; j++)     /* 使用 i 个骰子最小点数为 i */
+                for (int k = 1; k <= face && k < j; k++)
+                    dp[i][j] += dp[i - 1][j - k];
+
+        final double totalNum = Math.pow(6, n);
+        List<Map.Entry<Integer, Double>> ret = new ArrayList<>();
+        for (int i = n; i <= pointNum; i++)
+            ret.add(new AbstractMap.SimpleEntry<>(i, dp[n][i] / totalNum));
+
+        return ret;
+    }
+}
+
+/*
+使用一个二维数组 dp 存储点数出现的次数，其中 dp[i][j] 表示前 i 个骰子产生点数 j 的次数。
+
+空间复杂度：O(N2)
+
+dp[i][j] = dp[i - 1][j - (1 ~ min(j - 1,face) )]
+ */
